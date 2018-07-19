3a0370822f8261f4cb90d6ebc5289c2015a8197f
Cepilloman <farango@ideassimples.com.co>
2018-07-19 16:51:05 -0500

    Elaboracion de los tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+//pages
+import { PendientesPage } from '../pages/pendientes/pendientes.component'
+import { TerminadosPage } from '../pages/terminados/terminados.component'
+
+//services
+import { DeseosService } from '../services/deseos.service';
+
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    TerminadosPage,
+    PendientesPage
   ],
   imports: [
     BrowserModule,
@@ -26,14 +29,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    TerminadosPage,
+    PendientesPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    DeseosService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
---
+++ src/services/deseos.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class DeseosService {
+
+
+  constructor() {
+    console.log(123)
+  }
+}
